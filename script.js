96714257c94704bdb148bb4a0b704166b1e9d641
Nathan Brailsford <nbrailsford@msn.com>
2021-11-20 12:45:46 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//revenue in millions
+let heightArray = [0,5,22,51,100,176,310,497,749,1000,1300,1600.2270,3000,4000,5000,6660,8400,10548,13300,17000,0]
+
+//create smaller array
+function downsize(num){
+    return num*.05;
+}
+let smallHeight = heightArray.map(downsize)
+console.log(smallHeight)
+
+//Crate an array of element ids
+let graphArray = [];
+for (let i = 1; i < 21; i++){
+    graphArray.push(i.toString());
+}
+
+//add graph height to each element
+function addHeight(){
+
+    for(let i = 0; i < 20; i++){
+        document.getElementById(graphArray[i]).style.height = smallHeight[i].toString() + 'px'
+    }
+}
+
+
+
